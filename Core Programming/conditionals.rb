f01d457e99a964eb71781711dc8f5966dc360ca0
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-02 23:07:19 -0500

    conditionals 3 solution

--- Core Programming/conditionals.rb
+++ Core Programming/conditionals.rb
@@ -1,24 +1,32 @@
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-x = 10
-if x == 10
-  p 0
-else 
-  p -1
-end
+# x = 10
+# if x == 10
+#   p 0
+# else 
+#   p -1
+# end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-number = 5
-if number < 10
-  p -1
-elsif number > 10
-  p 1
-elsif number == 10
-  p 0
-end
+# number = 5
+# if number < 10
+#   p -1
+# elsif number > 10
+#   p 1
+# elsif number == 10
+#   p 0
+# end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+x = 2
+y = 4
+if x < 10 && y < 10
+  p 10
+else
+  p 0
+end
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
