551c49f9659639fe4378b77369f48612fdad6bdc
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-07 23:21:56 -0500

    strings 8

--- Core Programming/strings.rb
+++ Core Programming/strings.rb
@@ -48,6 +48,10 @@
 
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
+title = "Le Petit Prince"
+author = "Antoine de Saint-Exupéry"
+p "My favorite book is #{title} by #{author}."
+
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
