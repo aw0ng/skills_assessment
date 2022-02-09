ffe1b799f200199749d195e5a07a4ff12965e06c
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-08 23:25:04 -0500

    methods 2

--- Core Programming/methods.rb
+++ Core Programming/methods.rb
@@ -1,10 +1,16 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-puts "Enter a word"
-input = gets.chomp
-p input.upcase
+# puts "Enter a word"
+# input = gets.chomp
+# p input.upcase
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+puts "Enter a number"
+input = gets.chomp
+if input.to_i > 100
+  p "That's a big number"
+end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
