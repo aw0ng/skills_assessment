f51c774161c43cea1b2f06751d3445e055df2f84
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-10 22:10:16 -0500

    arrays 4

--- Core Programming/arrays.rb
+++ Core Programming/arrays.rb
@@ -13,14 +13,20 @@
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
-numbers = [1, 2, 3, 4, 5]
-i = 0
-while i < numbers.length
-  p numbers[i]
-  i += 1
-end
+# numbers = [1, 2, 3, 4, 5]
+# i = 0
+# while i < numbers.length
+#   p numbers[i]
+#   i += 1
+# end
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+numbers = [1]
+numbers << 2
+numbers << 3
+numbers << 4
+p numbers
 
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
 
