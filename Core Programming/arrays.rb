cfdecf4cf68b7e007dd93e5b90959a0a401bf3f0
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-10 22:29:01 -0500

    arrays 9

--- Core Programming/arrays.rb
+++ Core Programming/arrays.rb
@@ -51,11 +51,18 @@
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
 
-numbers = [11, 12, 13, 14, 15]
-numbers[0] = numbers[0] * 10
-p numbers
+# numbers = [11, 12, 13, 14, 15]
+# numbers[0] = numbers[0] * 10
+# p numbers
 
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [6, 7]
+i = 0
+while i < numbers.length
+  p numbers[i]
+  i += 1
+end
 
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
 
