08a5ce2c8b3f556fe94d1b8d9a7e82f0aa273e81
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-10 22:09:18 -0500

    arrays 3

--- Core Programming/arrays.rb
+++ Core Programming/arrays.rb
@@ -7,11 +7,18 @@
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-letters = ["a", "b", "c", "d"]
-letters[1] = 1
-p letters
+# letters = ["a", "b", "c", "d"]
+# letters[1] = 1
+# p letters
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [1, 2, 3, 4, 5]
+i = 0
+while i < numbers.length
+  p numbers[i]
+  i += 1
+end
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
