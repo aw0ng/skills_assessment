c2fac72004b1a22c475ebdbeb4de99859a07943f
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-14 00:00:25 -0500

    functions 8

--- Core Programming/functions.rb
+++ Core Programming/functions.rb
@@ -49,13 +49,19 @@
 
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
 
-def stringify(number)
-  return number.to_s
-end
+# def stringify(number)
+#   return number.to_s
+# end
 
-p stringify(3)
+# p stringify(3)
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+def five_times(string)
+  return string * 5
+end
+
+p five_times("orange")
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
 
