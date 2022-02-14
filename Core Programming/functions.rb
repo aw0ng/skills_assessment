e3e79d8264a6b849a7076d2b5f6aa589111459e3
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-13 23:59:39 -0500

    functions 7

--- Core Programming/functions.rb
+++ Core Programming/functions.rb
@@ -40,14 +40,20 @@
 
 # 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
-def combined(string1, string2, string3)
-  # return string1 + " " + string2 + " " + string3
-  return "#{string1} #{string2} #{string3}"
-end
+# def combined(string1, string2, string3)
+#   # return string1 + " " + string2 + " " + string3
+#   return "#{string1} #{string2} #{string3}"
+# end
 
-p combined("apple", "banana", "orange")
+# p combined("apple", "banana", "orange")
 
 # 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+def stringify(number)
+  return number.to_s
+end
+
+p stringify(3)
 
 # 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
