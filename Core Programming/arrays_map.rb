7bebe98b75a2542020c14e3193c741eae81306ca
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-14 22:58:07 -0500

    arrays map 7

--- Core Programming/arrays_map.rb
+++ Core Programming/arrays_map.rb
@@ -138,30 +138,52 @@
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
-people = [{name: "Lexa", age: 34}, {name: "Anna", age: 37}, {name: "Ollie", age: 0}]
-ages = []
-i = 0
-while i < people.length
-  ages << people[i][:age]
-  i += 1
-end
-p ages
+# people = [{name: "Lexa", age: 34}, {name: "Anna", age: 37}, {name: "Ollie", age: 0}]
+# ages = []
+# i = 0
+# while i < people.length
+#   ages << people[i][:age]
+#   i += 1
+# end
+# p ages
 
-people2 = [{name: "Lexa", age: 34}, {name: "Anna", age: 37}, {name: "Ollie", age: 0}]
-ages2 = []
-people2.each do |people|
-  ages2 << people[:age]
-end
-p ages2
+# people2 = [{name: "Lexa", age: 34}, {name: "Anna", age: 37}, {name: "Ollie", age: 0}]
+# ages2 = []
+# people2.each do |people|
+#   ages2 << people[:age]
+# end
+# p ages2
 
-people3 = [{name: "Lexa", age: 34}, {name: "Anna", age: 37}, {name: "Ollie", age: 0}]
-ages3 = people3.map do |people|
-  people[:age]
-end
-p ages3
+# people3 = [{name: "Lexa", age: 34}, {name: "Anna", age: 37}, {name: "Ollie", age: 0}]
+# ages3 = people3.map do |people|
+#   people[:age]
+# end
+# p ages3
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+nums = [3, 4, 5]
+new_nums = []
+i = 0
+while i < nums.length
+  new_nums << nums[i] / 2.0
+  i += 1
+end
+p new_nums
+
+nums2 = [3, 4, 5]
+new_nums2 = []
+nums2.each do |num|
+  new_nums2 << num / 2.0
+end
+p new_nums2
+
+nums3 = [3, 4, 5]
+new_nums3 = nums3.map do |num|
+  num / 2.0
+end
+p new_nums3
 
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
