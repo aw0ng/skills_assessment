85ad19266ead8cb1c10b2f2ce94c5aec7a1c45c0
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-14 22:25:40 -0500

    arrays map 2

--- Core Programming/arrays_map.rb
+++ Core Programming/arrays_map.rb
@@ -3,35 +3,61 @@
 #  1. Start with an array of numbers and create a new array with each number times 3.
 #     For example, [1, 2, 3] becomes [3, 6, 9].
 
-numbers = [1, 2, 3]
-new_numbers = []
+# numbers = [1, 2, 3]
+# new_numbers = []
 
-i = 0
-while i < numbers.length
-  new_numbers << numbers[i] * 3
-  i += 1
-end
+# i = 0
+# while i < numbers.length
+#   new_numbers << numbers[i] * 3
+#   i += 1
+# end
 
-p new_numbers
+# p new_numbers
 
-numbers2 = [1, 2, 3]
-new_numbers2 = []
+# numbers2 = [1, 2, 3]
+# new_numbers2 = []
 
-numbers2.each do |number|
-  new_numbers2 << number * 3
-end
+# numbers2.each do |number|
+#   new_numbers2 << number * 3
+# end
 
-p new_numbers2
+# p new_numbers2
 
-numbers3 = [1, 2, 3]
-new_numbers3 = numbers3.map do |number|
-  number * 3
-end
+# numbers3 = [1, 2, 3]
+# new_numbers3 = numbers3.map do |number|
+#   number * 3
+# end
 
-p new_numbers3
+# p new_numbers3
 
 #  2. Start with an array of strings and create a new array with each string upcased.
 #     For example, ["hello", "goodbye"] becomes ["HELLO", "GOODBYE"].
+
+strings = ["apple", "orange", "banana"]
+up_strings = []
+i = 0
+while i < strings.length
+  up_strings << strings[i].upcase
+  i += 1
+end
+
+p up_strings
+
+strings2 = ["apple", "orange", "banana"]
+up_strings2 = []
+
+strings2.each do |string|
+  up_strings2 << string.upcase
+end
+
+p up_strings2
+
+strings3 = ["apple", "orange", "banana"]
+up_strings3 = strings3.map do |string|
+  string.upcase
+end
+
+p up_strings3
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
