a6746cb0c966fd5ae50ef4cb7a2076ad70f5c68d
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-20 23:22:42 -0500

    converting data 4

--- Core Programming/converting_data.rb
+++ Core Programming/converting_data.rb
@@ -27,34 +27,28 @@
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
 
-string = "bookkeeper"
-string_hash = {}
-i = 0
-while i < string.length
-  letter = string[i]
-  if string_hash[letter] == nil
-    string_hash[letter] = 0
-  end
-  string_hash[letter] += 1
-  i += 1
-end
-p string_hash
-
-# word = "bookkeeper"
-# letter_frequencies = {}
-# index = 0
-# while index < word.length
-#   letter = word[index]
-#   if letter_frequencies[letter] == nil
-#     letter_frequencies[letter] = 0
+# string = "bookkeeper"
+# string_hash = {}
+# i = 0
+# while i < string.length
+#   letter = string[i]
+#   if string_hash[letter] == nil
+#     string_hash[letter] = 0
 #   end
-#   letter_frequencies[letter] += 1
-#   index += 1
+#   string_hash[letter] += 1
+#   i += 1
 # end
-# p letter_frequencies
+# p string_hash
 
 #  4. Convert a hash into an array of arrays.
 #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+
+hash = {"chair" => 100, "book" => 14} 
+hash_array = []
+hash.each do | item, price |
+  hash_array << [item, price]
+end
+p hash_array
 
 #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
