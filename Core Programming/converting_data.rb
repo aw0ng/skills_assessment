23f00fa4ba0d4116d85983836f50de9bade45b61
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-20 23:13:55 -0500

    converting data 2

--- Core Programming/converting_data.rb
+++ Core Programming/converting_data.rb
@@ -1,21 +1,28 @@
 #  1. Convert an array of arrays into a hash.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
-pairs = [[1, 3], [8, 9], [2, 16]]
-pairs_hash = {}
-i = 0
-while i < pairs.length
-  key = pairs[i][0]
-  value = pairs[i][1]
-  pairs_hash[key] = value
-  i += 1
-end
-p pairs_hash
+# pairs = [[1, 3], [8, 9], [2, 16]]
+# pairs_hash = {}
+# i = 0
+# while i < pairs.length
+#   key = pairs[i][0]
+#   value = pairs[i][1]
+#   pairs_hash[key] = value
+#   i += 1
+# end
+# p pairs_hash
 
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
-
+colors = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+colors_hash = {}
+i = 0
+while i < colors.length
+  colors_hash[colors[i][:id]] = colors[i]
+  i += 1
+end
+p colors_hash
 
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
