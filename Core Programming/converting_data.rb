621506a0c59a75a956b43a281fc7e2747f6a266b
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-20 23:26:21 -0500

    converting data 5

--- Core Programming/converting_data.rb
+++ Core Programming/converting_data.rb
@@ -43,15 +43,23 @@
 #  4. Convert a hash into an array of arrays.
 #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
 
-hash = {"chair" => 100, "book" => 14} 
-hash_array = []
-hash.each do | item, price |
-  hash_array << [item, price]
-end
-p hash_array
+# hash = {"chair" => 100, "book" => 14} 
+# hash_array = []
+# hash.each do | item, price |
+#   hash_array << [item, price]
+# end
+# p hash_array
 
 #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
+
+people = {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}}
+people_array = []
+people.each do | id, person |
+  person[:id] = id
+  people_array << person
+end
+p people_array
 
 #  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
 #     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
