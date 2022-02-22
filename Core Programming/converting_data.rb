e12bf54573679daa50bf040e7066956d7d79c66b
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2022-02-21 23:06:06 -0500

    converting data 8

--- Core Programming/converting_data.rb
+++ Core Programming/converting_data.rb
@@ -80,16 +80,31 @@
 #  7. Convert a hash into a flat array containing all the hash’s keys and values.
 #     For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
 
-hash =  {"a" => 1, "b" => 2, "c" => 3, "d" => 4} 
-flat_hash = []
-hash.each do | key, value |
-  flat_hash << key
-  flat_hash << value
-end
-p flat_hash
+# hash =  {"a" => 1, "b" => 2, "c" => 3, "d" => 4} 
+# flat_hash = []
+# hash.each do | key, value |
+#   flat_hash << key
+#   flat_hash << value
+# end
+# p flat_hash
 
 #  8. Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
 #     For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+
+price_hash = { "chair" => 75, "book" => 15 }
+items = [{ name: "chair", color: "red", weight: 10 }, { name: "book", color: "black", weight: 1 }]
+combined_hash = {}
+index = 0
+while index < items.length
+  item = items[index]
+  name = item[:name]
+  color = item[:color]
+  weight = item[:weight]
+  price = price_hash[name]
+  combined_hash[name] = { price: price, color: color, weight: weight }
+  index += 1
+end
+p combined_hash
 
 #  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 #     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
